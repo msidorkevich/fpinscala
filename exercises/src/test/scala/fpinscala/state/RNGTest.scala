8ae6f3ee960e9eff14fb9ff9cf8d02514513a33b
Mikhail Sidorkevich <michael.sidorkevich@gmail.com>
2017-06-17 20:13:47 +0300

    test refactor: moving RNG creation to before method

--- exercises/src/test/scala/fpinscala/state/RNGTest.scala
+++ exercises/src/test/scala/fpinscala/state/RNGTest.scala
@@ -1,8 +1,16 @@
 package fpinscala.state
 
 import org.scalatest.FunSuite
+import org.scalatest.BeforeAndAfter
 
-class RNGTest extends FunSuite {
+class RNGTest extends FunSuite with BeforeAndAfter{
+
+  private var initialRNG = _
+
+  before {
+    initialRNG = RNG.Simple(1)
+  }
+
   test("nonNegativeInt") {
     (-1000 to 1000).foreach(i => {
       val initialRNG = RNG.Simple(i)
@@ -13,19 +21,16 @@ class RNGTest extends FunSuite {
   }
 
   test("nonNegativeInt corner case") {
-    val initialRNG = RNG.Simple(-2071084361)
     val (nonNegative, _) = RNG.nonNegativeInt(initialRNG)
     assert(nonNegative >= 0)
   }
 
   test("double") {
-    val initialRNG = RNG.Simple(1)
     val (double, _) = RNG.double(initialRNG)
     assert(double.isWhole() === false)
   }
 
   test("intDouble") {
-    val initialRNG = RNG.Simple(1)
     val ((int, double), _) = RNG.intDouble(initialRNG)
 
     assert(int.isWhole() === true)
@@ -33,7 +38,6 @@ class RNGTest extends FunSuite {
   }
 
   test("doubleInt") {
-    val initialRNG = RNG.Simple(1)
     val ((double, int), _) = RNG.doubleInt(initialRNG)
 
     assert(int.isWhole() === true)
@@ -41,7 +45,6 @@ class RNGTest extends FunSuite {
   }
 
   test("double3") {
-    val initialRNG = RNG.Simple(1)
     val ((d1, d2, d3), _) = RNG.double3(initialRNG)
 
     assert(d1.isWhole() === false)
